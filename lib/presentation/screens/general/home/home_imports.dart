acea23ab548a95a7772779bfeb73884be347bf4c
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-25 23:32:57 +0530

    categories screen and tag screen completed

--- lib/presentation/screens/general/home/home_imports.dart
+++ lib/presentation/screens/general/home/home_imports.dart
@@ -1,8 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:blog_app/core/constants/my_assets.dart';
 import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:velocity_x/velocity_x.dart';
-
+part 'home_details.dart';
 part 'home.dart';
 part 'home_view_model.dart';
